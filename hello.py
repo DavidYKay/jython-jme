d2b87805f5e159058f89ff26072b05867d5e4a74
David Young-chan Kay <davidykay@gmail.com>
2018-12-24 15:58:15 -0800

    adding a bunch of LWJGL deps.

--- hello.py
+++ hello.py
@@ -1,22 +1,23 @@
 import sys
 
-
-# sys.path.append("/home/dk/workspace/current/mvr/libs/jme3-core-3.3.0-SNAPSHOT.jar")
-# sys.path.append("/home/dk/workspace/current/mvr/libs/jme3-desktop-3.3.0-SNAPSHOT.jar")
-# sys.path.append("/home/dk/workspace/current/mvr/libs/jme3-lwjgl3-3.3.0-SNAPSHOT.jar")
-
 sys.path.append("libs/jme3-core-3.3.0-SNAPSHOT.jar")
 sys.path.append("libs/jme3-desktop-3.3.0-SNAPSHOT.jar")
 sys.path.append("libs/jme3-lwjgl3-3.3.0-SNAPSHOT.jar")
 
+sys.path.append("libs/lwjgl.jar")
+sys.path.append("libs/lwjgl-natives-linux.jar")
+sys.path.append("libs/lwjgl-opengl.jar")
+sys.path.append("libs/lwjgl-opengl-natives-linux.jar")
+sys.path.append("libs/lwjgl-glfw.jar")
+sys.path.append("libs/lwjgl-glfw-natives-linux.jar")
+
 import java.io.IOException
 
 from com.jme3.app import SimpleApplication
-# import com.jme3.app.SimpleApplication
-# import com.jme3.material.Material
-# import com.jme3.scene.Geometry
-# import com.jme3.scene.shape.Box
-# import com.jme3.math.ColorRGBA
+from com.jme3.scene.shape import Box
+from com.jme3.scene import Geometry
+from com.jme3.material import Material
+from com.jme3.math import ColorRGBA
 
 #  Sample 1 - how to get started with the most simple JME 3 application.
 #  Display a blue 3D cube and view from all sides by
